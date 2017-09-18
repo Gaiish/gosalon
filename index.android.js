bbcad20aae6780a89d5b4edc8625192d5f7722fd
tugumeandree <andrewtugume2@gmail.com>
2017-09-18 15:56:44 +0300

    added branch

--- index.android.js
+++ index.android.js
@@ -11,7 +11,7 @@ export default class gosalon extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome
+          Welcome to my branch
         </Text>
         <Text style={styles.instructions}>
           great
