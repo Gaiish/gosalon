635d6a0cfdddb073e4fb516e143f5a0c7977dc50
Gadi <gdishim@gmail.com>
2017-09-18 16:07:52 +0300

    init

--- index.android.js
+++ index.android.js
@@ -11,7 +11,7 @@ export default class gosalon extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome
+          Happy hacking!
         </Text>
         <Text style={styles.instructions}>
           Cool
